20d66067cc3d72ce024be1499a083e9e01087979
Noval Bayu Setiawan <novalbayu28@gmail.com>
2021-09-08 15:28:40 +0800

    + Macro : WhereLike

--- src/AutocrudServiceProvider.php
+++ src/AutocrudServiceProvider.php
@@ -9,6 +9,27 @@ class AutocrudServiceProvider extends ServiceProvider{
     {
         # code...
         // dd(123123);
+        Builder::macro('whereLike', function ($attributes, string $searchTerm) {            
+            $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+                foreach (Arr::wrap($attributes) as $attribute) {
+                    $query->when(
+                        Str::contains($attribute, '.'),
+                        function (Builder $query) use ($attribute, $searchTerm) {
+                            [$relationName, $relationAttribute] = explode('.', $attribute);
+        
+                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
+                                $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
+                            });
+                        },
+                        function (Builder $query) use ($attribute, $searchTerm) {
+                            $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                        }
+                    );
+                }
+            });
+        
+            return $this;
+        });
     }
 
     public function register()
